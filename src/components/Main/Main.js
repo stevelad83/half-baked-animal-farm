18cee517dfa8a44958b4b0740d9763ba3e4b80d5
Steven Bryant <stevelad_83@yahoo.com>
2023-01-09 18:06:06 -0500

    positioned animals

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -6,9 +6,16 @@ import { animals } from '../../data.js';
 
 export default function Main() {
   return (
-    <main className="background" style={{ backgroundImage: `url(${background})` }}>
+    <main className="main" style={{ backgroundImage: `url(${background})` }}>
       {animals.map((animal) => (
-        <Animal key={animal.type} name={animal.name} image={animal.image} sound={animal.says} />
+        <Animal
+          key={animal.type}
+          name={animal.name}
+          image={animal.image}
+          sound={animal.says}
+          top={animal.top}
+          left={animal.left}
+        />
       ))}
     </main>
   );
