c0972105d1f4b1d2d120b93a8b9318c8826b8577
Steven Bryant <stevelad_83@yahoo.com>
2023-01-09 17:39:19 -0500

    added animal component and map

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,3 +1,9 @@
-export default function Animal() {
-  return <></>;
+export default function Animal(props) {
+  return (
+    <div className="animal">
+      <img src={`${props.name}.svg`} />
+      <h2>{props.name}</h2>
+      <p>{props.says}</p>
+    </div>
+  );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import './Main.css';
 import background from '../../background.png';
+import Animal from '../Animal/Animal.js';
+import { animals } from '../../data.js';
 
 export default function Main() {
-  return <main className="background" style={{ backgroundImage: `url(${background})` }}></main>;
+  return (
+    <main className="background" style={{ backgroundImage: `url(${background})` }}>
+      {animals.map((animal) => (
+        <Animal key={animal.type} name={animal.name} image={animal.image} sound={animal.says} />
+      ))}
+    </main>
+  );
 }
